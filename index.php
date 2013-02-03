c6697c2520db244f80cbdf867ef1ba2890e5b819
ringmaster <epithet@gmail.com>
2013-02-03 12:34:36 -0500

    Connect up some database.

--- index.php
+++ index.php
@@ -8,7 +8,18 @@ use Microsite\Renderers\PlainRenderer;
 
 include 'microsite.phar';
 
-$app = new App();
+class TinyDocsApp extends App
+{
+	/**
+	 * @return \Microsite\DB\PDO\DB The database object
+	 */
+	public function db() {
+		$args = func_get_args();
+		return $this->dispatch_object('db', $args);
+	}
+}
+
+$app = new TinyDocsApp();
 
 // Assign a directory in which templates can be found for rendering
 $app->template_dirs = [
@@ -16,7 +27,7 @@ $app->template_dirs = [
 ];
 
 $app->share('db', function() {
-	$db = new DB('sqlite:' . __DIR__ . '/tinydocs.db');
+	$db = new \Microsite\DB\PDO\DB('sqlite:' . __DIR__ . '/tinydocs.db');
 	return $db;
 });
 
@@ -24,10 +35,16 @@ $app->share('db', function() {
  * Basic home page.
  * Set the view to a home.php view provided in the view directory
  */
-$generate_page = function(Response $response, Request $request, App $app) {
+$generate_page = function(Response $response, Request $request, TinyDocsApp $app) {
 	$response['project'] = 'TinyDocs';
 	$page_renderer = MarkdownRenderer::create($app->template_dirs(), $app);
-	$response['page'] = $page_renderer->render($request['page'] . '.md');
+
+	$page = $app->db()->row('SELECT * FROM pages WHERE slug = :slug', ['slug' => $request['page']]);
+	$rev = $app->db()->row('SELECT * FROM revisions WHERE page_id = :page_id AND rev = (SELECT MAX(rev) FROM revisions WHERE page_id = :page_id);', ['page_id' => $page['id']]);
+	$page = array_merge($page->ary(), $rev->ary());
+
+	$response['title'] = $page['title'];
+	$response['page'] = $page_renderer->render($page['content']);
 	$response['editlink'] = $app->get_url('page_edit', $request);
 	return $response->render('home.php');
 };
@@ -35,8 +52,9 @@ $generate_page = function(Response $response, Request $request, App $app) {
 $app->route(
 	'home',
 	'/',
-	function(Request $request) {
-		$request['page'] = 'default';
+	function(Request $request, TinyDocsApp $app) {
+		$first_page = $app->db()->val('SELECT slug FROM pages WHERE chapter_type = "page" ORDER BY sort_order ASC LIMIT 1');
+		$request['page'] = $first_page;
 	},
 	$generate_page
 );
