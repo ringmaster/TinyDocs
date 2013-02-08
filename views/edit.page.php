2f5f77bd15f429784cf384867cda997f7a06e570
ringmaster <epithet@gmail.com>
2013-02-07 22:07:51 -0500

    Add drop location on edit page.

--- views/edit.page.php
+++ views/edit.page.php
@@ -3,3 +3,6 @@
 <textarea id="edit_content" name="content"><?= htmlspecialchars($content) ?></textarea>
 <input type="submit" class="btn" value="Submit">
 </form>
+<div id="files">
+	<span id="files_notice">Drop files here...</span>
+</div>
